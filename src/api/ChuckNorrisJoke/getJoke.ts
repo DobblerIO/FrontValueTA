f5129099af6673edb008850538b0f70ae0fc6107
DobblerIO <marco.k.prins@gmail.com>
2026-07-14 00:55:22 +0200

    chore: remove oldest joke (#7)

--- src/api/ChuckNorrisJoke/getJoke.ts
+++ src/api/ChuckNorrisJoke/getJoke.ts
@@ -3,6 +3,8 @@ import { ApiClient } from "../ApiClient";
 export interface ChuckNorrisJokeData {
     id: string; // "FIc_JItqQxiqQSwcoU2hzg"
     value: string; // "When Chuck Norris goes clubbing, the authorities turn a blind eye on all the people he clubs to death."
+    created_at: string; // "2020-01-05 13:42:26.194739"
+    updated_at: string; // "2020-01-05 13:42:26.194739"
     
     /* Exist within the data, but redundant within the current scope */
     // icon_url: string; // "https://api.chucknorris.io/img/avatar/chuck-norris.png"
